23af30a95f6eeede9dadd654633015d302c51491
AlifSakib <alifsakib@gmail.com>
2023-01-11 00:46:24 +0600

    implemented link , replace and anchore

--- hello-world/pages/index.js
+++ hello-world/pages/index.js
@@ -1,11 +1,13 @@
-import React from "react";
+import Link from "next/link";
 
-const index = () => {
+const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/product">Product</Link>
     </div>
   );
 };
 
-export default index;
+export default Home;
--- hello-world/pages/product/index.js
+++ hello-world/pages/product/index.js
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const ProductList = () => {
   return (
     <div>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href="/product/1">
+        <h2>Product 1</h2>
+      </Link>
+      <Link href="/product/2">
+        <h2>Product 2</h2>
+      </Link>
+      <Link href="/product/3">
+        <h2>Product 3</h2>
+      </Link>
     </div>
   );
 };
