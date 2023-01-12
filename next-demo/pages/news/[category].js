71b469fd97f3f6289787a26f7b2fd4fb2570447a
AlifSakib <alifsakib@gmail.com>
2023-01-12 20:12:00 +0600

    exploring context , req, res, query

--- next-demo/pages/news/[category].js
+++ next-demo/pages/news/[category].js
@@ -19,14 +19,17 @@ function ArticleListByCategory({ news, category }) {
 export default ArticleListByCategory;
 
 export async function getServerSideProps(context) {
-  console.log(context);
-  const { params } = context;
+  const { params, req, res, query } = context;
+  console.log(query);
+  console.log(req.headers.cookie);
+  res.setHeader("Set-Cookie", ["name=Alif"]);
   console.log(params);
   const { category } = params;
   const response = await fetch(
     `http://localhost:4000/news?category=${category}`
   );
   const data = await response.json();
+  console.log(`Pre-rendering News Articles for category ${category}`);
 
   return {
     props: {
